5020d6c24169efdaee804d10a30b87c4b541cccf
SujanBoopathy <sujansaradha@gmail.com>
2024-08-18 22:07:38 +0530

    ui changes

--- EmployeeManager/client/src/app/app.component.ts
+++ EmployeeManager/client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+    public employees : Employee[] = [];
+
+    public constructor(private employeeService : EmployeeService){}
+
+
+    ngOnInit(): void {
+      this.getEmployees();
+    }
+
+
+    public getEmployees() : void {
+        this.employeeService.getEmployees().subscribe( (employeeResponse : Employee[]) => {
+          this.employees = employeeResponse;
+        }),
+        (error : HttpErrorResponse) => {
+          alert(error.message);
+        }
+    }
 }
