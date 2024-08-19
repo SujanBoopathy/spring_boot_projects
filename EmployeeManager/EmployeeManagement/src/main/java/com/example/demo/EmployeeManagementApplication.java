68889cbc717d0e76f23510429bd59fc7cdeff1af
SujanBoopathy <sujansaradha@gmail.com>
2024-08-19 09:50:53 +0530

    client changes

--- EmployeeManager/EmployeeManagement/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManager/EmployeeManagement/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:4200"); // Angular app URL
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
 	}
 
 }
