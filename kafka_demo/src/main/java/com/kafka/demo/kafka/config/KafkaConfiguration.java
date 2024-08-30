7bb1e073e80efee0b2c713317714b4b7fcc4beca
SujanBoopathy <sujansaradha@gmail.com>
2024-08-30 21:36:10 +0530

    kafka topic

---
+++ kafka_demo/src/main/java/com/kafka/demo/kafka/config/KafkaConfiguration.java
@@ -0,0 +1,14 @@
+package com.kafka.demo.kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+    @Bean
+    public NewTopic sujanTopic(){
+        return TopicBuilder.name("sujan").build();
+    }
+}
